a6194103eae75377620f2ba613d0979c4fe3a250
Jitender Bhati <jitender_bhati@epam.com>
2020-08-14 20:39:57 +0530

    Secure Service with Auth Server

--- user_service/src/main/java/com/epam/user_service/UserServiceApplication.java
+++ user_service/src/main/java/com/epam/user_service/UserServiceApplication.java
@@ -10,9 +10,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableResourceServer
 public class UserServiceApplication {
 
     public static void main(String[] args) {
